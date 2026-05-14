1b3ab610fcb99d9bc8487937ad47af8fe25e2ff8
Zacharia316 <zappyblues234@gmail.com>
2026-05-14 08:40:19 +0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CodrisAcademy from "./CodrisAcademy.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CodrisAcademy />
+  </React.StrictMode>
+);
